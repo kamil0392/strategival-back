cafc23a904bc1537c0dd1162fe096443b264317d
Kamil Kandefer <kamil.0392@gmail.com>
2020-03-10 19:19:22 +0100

    Add lombok dependencies

--- src/main/java/com/strategival/controller/FirstController.java
+++ src/main/java/com/strategival/controller/FirstController.java
@@ -1,13 +1,19 @@
 package com.strategival.controller;
 
+import com.strategival.model.FirstModel;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController("/")
 public class FirstController {
 
     @GetMapping
     public String helloStrategival() {
-        return "Hello this is strategival!!!";
+        log.info("++++Test log1++++++++");
+        FirstModel model = new FirstModel("name1");
+
+        return "Hello this is strategival!!!" + model.getName();
     }
 }
